23452a0ad2022f5a9925dc7b5ec35186cbf18e8a
elbertkhoe <elbert.422022016@civitas.ukrida.ac.id>
2023-11-10 10:03:59 +0700

    OAuth Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
-Route::get('login', function () {
-    return view('auth.user.login');
-})->name('login');
+})->name('welcome');
+// Route::get('login', function () { // using the one in auth.php
+    // return view('auth.user.login');
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProvidercallback'])->name('user.google.callback');
 Route::get('dashboard', function () {
     return view('user.dashboard');
 })->name('dashboard');
@@ -28,3 +31,5 @@ Route::get('checkout/success', function () {
 Route::get('checkout/{camp}', function () {
     return view('checkout.create');
 });
+
+require __DIR__.'/auth.php';
